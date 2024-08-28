2305302f6d0f05aebc208c70b2115fc0c3d58dbf
Git RX <95379583+moniroul@users.noreply.github.com>
2024-08-29 02:07:00 +0600

    user info dynamic
    user info page create and data add on list and view listview builder

--- contact_list/lib/Homepage.dart
+++ contact_list/lib/Homepage.dart
@@ -10,78 +10,130 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  
+  List<UserModel> users = [];
 
-  List<UserModel> users = [
-    UserModel(
-      id: 1,
-      phone: 1234567890,
-      name: "Sahil",
-      email: "sahil@123",
-      profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-      about:
-          "I am sahil  I am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahilI am sahil ",
-    ),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-    UserModel(
-        id: 1,
-        phone: 1234567890,
-        name: "Sahil",
-        email: "sahil@123",
-        profileImage: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
-        about: "this my about data"),
-  ];
- 
   String dafultImage =
       "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=626&ext=jpg";
-      
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController aboutController = TextEditingController();
+
+  UserDataAdd() {
+
+    UserModel user = UserModel(
+      id: users.length + 1,
+      name: nameController.text,
+      email: emailController.text,
+      phone: int.parse(phoneController.text),
+      about: aboutController.text,
+      profileImage: dafultImage,
+    );
+
+    setState(() {
+      users.add(user);
+    });
+
+    nameController.clear();
+    emailController.clear();
+    phoneController.clear();
+    aboutController.clear();
+  }
+
+  Widget UserAdd() {
+    return Dialog(
+      child: Container(
+        height: 500,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "User Info Add",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: phoneController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "phone",
+                  ),
+                  maxLength: 11,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: aboutController,
+                  maxLines: 3,
+                  maxLength: 100,
+                  decoration: InputDecoration(
+                    hintText: "about",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                      color: Colors.green[200],
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Not now"),
+                    ),
+                    MaterialButton(
+                      color: Colors.green[200],
+                      onPressed: () {
+                        UserDataAdd();
+                        Navigator.pop(context);
+                      },
+                      child: Text("Save user"),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -125,6 +177,16 @@ class _HomepageState extends State<Homepage> {
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog(
+              context: context,
+              builder: (context) {
+                return UserAdd();
+              });
+        },
+        child: Icon(Icons.dialpad_sharp),
       ),
     );
   }
