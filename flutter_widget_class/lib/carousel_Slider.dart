ee61ccbec678a2b9d72746434acc2208bacdc6fd
Git RX <95379583+moniroul@users.noreply.github.com>
2024-09-24 23:52:09 +0600

    curousel slider add

---
+++ flutter_widget_class/lib/carousel_Slider.dart
@@ -0,0 +1,46 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class CarouselSliderExample extends StatefulWidget {
+  const CarouselSliderExample({Key? key}) : super(key: key);
+
+  @override
+  _CarouselSliderExampleState createState() => _CarouselSliderExampleState();
+}
+
+class _CarouselSliderExampleState extends State<CarouselSliderExample> {
+  List slidesdata = [1, 2, 3, 4, 5, 6, 7];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: CarouselSlider(
+        options: CarouselOptions(
+            height: 200.0,
+            initialPage: 4,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 5)),
+        items: slidesdata.map((item) {
+          return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://imgs.search.brave.com/WH4Rd2yx4oa7-Gw6pCPcmO8j9CBDuKUXuDKVEQm8oys/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuY3RmYXNzZXRz/Lm5ldC9ocmx0eDEy/cGw4aHEvMmNrYTEw/NE8wVU5tQlNCckcz/eHBqcS9hYmI0ZmY2/NjVmN2VkYjZmNjRi/ZjNiZTk2ODZlYWY2/ZC9oaXN0b3J5LWlt/YWdlcy1jYXJkLmpw/Zw"))),
+              child: Center(
+                child: Text(
+                  'Slide  ${item}',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800),
+                ),
+              ));
+        }).toList(),
+      ),
+    );
+  }
+}
--- flutter_widget_class/lib/main.dart
+++ flutter_widget_class/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_class/MainPage.dart';
 import 'package:flutter_widget_class/flutterTabBar.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "LWR Widget Class",
     debugShowCheckedModeBanner: false,
-    home: TabBarExample(),
+    home: MainPage(),
   ));
 }
